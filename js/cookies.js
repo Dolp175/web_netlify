db399bb253419aa29d99cdec672185a07595380e
Jordi Martinez Joyeux <jmartinezjoy@uoc.edu>
2023-05-25 20:24:27 +0200

    cookies banner added

---
+++ js/cookies.js
@@ -0,0 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (!getCookie("cookiesAccepted")) {
+        document.getElementById('cookieBanner').style.display = "flex";
+    }
+});
+
+function acceptCookies() {
+    setCookie("cookiesAccepted", "yes", 365);
+    document.getElementById('cookieBanner').style.display = "none";
+}
+
+function declineCookies() {
+    setCookie("cookiesAccepted", "no", 365);
+    document.getElementById('cookieBanner').style.display = "none";
+}
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
